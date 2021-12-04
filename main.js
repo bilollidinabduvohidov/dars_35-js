4077f4d04d3b335218904f4c1c6b9422141189eb
Abduvohidov Bilolliddin <bilol200414@gmail.com>
2021-12-04 16:25:59 +0500

    index

---
+++ main.js
@@ -0,0 +1,49 @@
+ //functions
+// console.log("salom".toUpperCase());
+
+// var myName1 = "bilol";
+
+// var myName = prompt("Ismizi kiriting!");
+
+// myName = myName.toLowerCase();
+
+// if(myName == myName1){
+//     console.log("Assalamu Aleikum");
+// } else{
+//     console.log("Assalamu Aleikum aka");
+// }
+
+// var confirmation = confirm( "sizning 100 USD bormi?" )
+
+// if(confirmation){
+//     var rozilik = confirm("biznes qilamiz")
+// } else{
+//     alert( "iwlasangiz bolmaydmi ?")
+// }
+
+// console.log(confirmation);
+
+//boolean ga oid narsalar  
+
+// console.log(0);
+
+// and va or
+
+// console.log(true || true);
+// console.log(false  || false);
+// true + true = true
+// true + false = true
+// false + true = true
+// false + false = false
+
+// console.log(false && false);
+// true * true = true
+// true * false = true
+// false * true = true
+// false * false = fals
+
+var x = 10
+var y = 20 
+var = 30
+
+console.log(x == z);
